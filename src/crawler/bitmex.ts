06554258c3be1aa8fc0680bb5131321af705ae36
soulmachine <soulmachine@gmail.com>
2020-05-24 13:03:01 -0700

    Fixed BitMEX Rate limit exceeded error, there is a number limit per connection

--- src/crawler/bitmex.ts
+++ src/crawler/bitmex.ts
@@ -5,13 +5,15 @@ import WebSocket from 'ws';
 import { ChannelType } from '../pojo/channel_type';
 import { BboMsg, KlineMsg, OrderBookMsg, OrderItem, TradeMsg } from '../pojo/msg';
 import { defaultMsgCallback, MsgCallback } from './index';
-import { debug, getChannels, initBeforeCrawl } from './util';
+import { chunkArray, debug, getChannels, initBeforeCrawl } from './util';
 
 // doc https://www.bitmex.com/app/wsAPI
 
 const EXCHANGE_NAME = 'BitMEX';
 
 const WEBSOCKET_ENDPOINT = 'wss://www.bitmex.com/realtime';
+
+const MAX_SUBSCRIPTIONS_NUM = 16; // just by guess, no official document
 
 const PERIOD_NAMES: { [key: string]: string } = {
   '1m': '1m',
@@ -160,270 +162,277 @@ export default async function crawl(
   // rawPair -> Map
   const idPriceMaps: { [key: string]: Map<number, number> } = {};
 
-  connect(
-    WEBSOCKET_ENDPOINT,
-    async (data) => {
-      const raw = data as string;
-      const obj = JSON.parse(raw);
+  const chunks = chunkArray(channels, MAX_SUBSCRIPTIONS_NUM);
 
-      if (obj.table === undefined) {
-        debug(obj);
-        return;
-      }
+  for (let i = 0; i < chunks.length; i += 1) {
+    const subscriptions = chunks[i];
 
-      const channel: string = obj.table;
-      const channelType = getChannelType(channel);
-      assert.ok(channelType);
+    connect(
+      WEBSOCKET_ENDPOINT,
+      async (data) => {
+        const raw = data as string;
+        const obj = JSON.parse(raw);
 
-      switch (channel) {
-        case 'quote': {
-          const tmp = obj as {
-            table: 'quote';
-            action: 'partial' | 'insert';
-            data: ReadonlyArray<{
-              timestamp: string;
-              symbol: string;
-              bidSize: number;
-              bidPrice: number;
-              askPrice: number;
-              askSize: number;
-            }>;
-          };
-
-          const rawBboMsg = tmp.data[tmp.data.length - 1]; // the last element is the newest quote
-          const market = marketMap.get(rawBboMsg.symbol)!;
-
-          const msg: BboMsg = {
-            exchange: EXCHANGE_NAME,
-            marketType,
-            pair: market.pair,
-            rawPair: rawBboMsg.symbol,
-            channel,
-            channelType,
-            timestamp: new Date(rawBboMsg.timestamp).getTime(),
-            raw: rawBboMsg,
-            bidPrice: rawBboMsg.bidPrice,
-            bidQuantity: calcQuantity(market, rawBboMsg.bidSize, rawBboMsg.bidPrice),
-            askPrice: rawBboMsg.askPrice,
-            askQuantity: calcQuantity(market, rawBboMsg.askSize, rawBboMsg.askPrice),
-          };
-
-          msgCallback(msg);
-          break;
+        if (obj.table === undefined) {
+          debug(obj);
+          return;
         }
-        case 'orderBookL2_25': {
-          const rawOrderBookMsg = obj as {
-            table: 'orderBookL2_25';
-            action: 'partial' | 'insert' | 'update' | 'delete';
-            data: ReadonlyArray<{
+
+        const channel: string = obj.table;
+        const channelType = getChannelType(channel);
+        assert.ok(channelType);
+
+        switch (channel) {
+          case 'quote': {
+            const tmp = obj as {
+              table: 'quote';
+              action: 'partial' | 'insert';
+              data: ReadonlyArray<{
+                timestamp: string;
+                symbol: string;
+                bidSize: number;
+                bidPrice: number;
+                askPrice: number;
+                askSize: number;
+              }>;
+            };
+
+            const rawBboMsg = tmp.data[tmp.data.length - 1]; // the last element is the newest quote
+            const market = marketMap.get(rawBboMsg.symbol)!;
+
+            const msg: BboMsg = {
+              exchange: EXCHANGE_NAME,
+              marketType,
+              pair: market.pair,
+              rawPair: rawBboMsg.symbol,
+              channel,
+              channelType,
+              timestamp: new Date(rawBboMsg.timestamp).getTime(),
+              raw: rawBboMsg,
+              bidPrice: rawBboMsg.bidPrice,
+              bidQuantity: calcQuantity(market, rawBboMsg.bidSize, rawBboMsg.bidPrice),
+              askPrice: rawBboMsg.askPrice,
+              askQuantity: calcQuantity(market, rawBboMsg.askSize, rawBboMsg.askPrice),
+            };
+
+            msgCallback(msg);
+            break;
+          }
+          case 'orderBookL2_25': {
+            const rawOrderBookMsg = obj as {
+              table: 'orderBookL2_25';
+              action: 'partial' | 'insert' | 'update' | 'delete';
+              data: ReadonlyArray<{
+                symbol: string;
+                id: number;
+                side: 'Sell' | 'Buy';
+                size: number;
+                price?: number;
+              }>;
+            };
+
+            const rawPair = rawOrderBookMsg.data[0].symbol;
+            const market = marketMap.get(rawOrderBookMsg.data[0].symbol)!;
+
+            if (!(rawPair in idPriceMaps)) {
+              idPriceMaps[rawPair] = new Map<number, number>();
+            }
+            const idPriceMap = idPriceMaps[rawPair];
+
+            if (rawOrderBookMsg.action === 'partial') {
+              idPriceMap.clear();
+            }
+
+            if (rawOrderBookMsg.action === 'insert' || rawOrderBookMsg.action === 'partial') {
+              rawOrderBookMsg.data
+                .filter((x) => x.price !== undefined)
+                .forEach((x) => {
+                  idPriceMap.set(x.id, x.price!);
+                });
+            }
+
+            const orderBookMsg: OrderBookMsg = {
+              exchange: EXCHANGE_NAME,
+              marketType,
+              pair: market.pair,
+              rawPair,
+              channel,
+              channelType,
+              timestamp: Date.now(),
+              raw: rawOrderBookMsg,
+              asks: [],
+              bids: [],
+              full: rawOrderBookMsg.action === 'partial',
+            };
+
+            const parse = (item: {
               symbol: string;
               id: number;
               side: 'Sell' | 'Buy';
               size: number;
               price?: number;
-            }>;
-          };
+            }): OrderItem => {
+              assert.ok(idPriceMap.has(item.id));
+              const price = idPriceMap.get(item.id)!;
 
-          const rawPair = rawOrderBookMsg.data[0].symbol;
-          const market = marketMap.get(rawOrderBookMsg.data[0].symbol)!;
+              const quantity =
+                rawOrderBookMsg.action === 'delete' ? 0 : calcQuantity(market, item.size, price);
+              const cost = quantity * price;
 
-          if (!(rawPair in idPriceMaps)) {
-            idPriceMaps[rawPair] = new Map<number, number>();
-          }
-          const idPriceMap = idPriceMaps[rawPair];
+              const result: OrderItem = {
+                price,
+                quantity,
+                cost,
+              };
+              return result;
+            };
 
-          if (rawOrderBookMsg.action === 'partial') {
-            idPriceMap.clear();
-          }
+            orderBookMsg.asks = rawOrderBookMsg.data
+              .filter((x) => x.side === 'Sell' && idPriceMap.has(x.id))
+              .map((x) => parse(x));
+            orderBookMsg.asks = rawOrderBookMsg.data
+              .filter((x) => x.side === 'Buy' && idPriceMap.has(x.id))
+              .map((x) => parse(x));
 
-          if (rawOrderBookMsg.action === 'insert' || rawOrderBookMsg.action === 'partial') {
-            rawOrderBookMsg.data
-              .filter((x) => x.price !== undefined)
-              .forEach((x) => {
-                idPriceMap.set(x.id, x.price!);
+            if (rawOrderBookMsg.action === 'delete') {
+              rawOrderBookMsg.data.forEach((x) => {
+                idPriceMap.delete(x.id);
               });
-          }
-
-          const orderBookMsg: OrderBookMsg = {
-            exchange: EXCHANGE_NAME,
-            marketType,
-            pair: market.pair,
-            rawPair,
-            channel,
-            channelType,
-            timestamp: Date.now(),
-            raw: rawOrderBookMsg,
-            asks: [],
-            bids: [],
-            full: rawOrderBookMsg.action === 'partial',
-          };
-
-          const parse = (item: {
-            symbol: string;
-            id: number;
-            side: 'Sell' | 'Buy';
-            size: number;
-            price?: number;
-          }): OrderItem => {
-            assert.ok(idPriceMap.has(item.id));
-            const price = idPriceMap.get(item.id)!;
-
-            const quantity =
-              rawOrderBookMsg.action === 'delete' ? 0 : calcQuantity(market, item.size, price);
-            const cost = quantity * price;
-
-            const result: OrderItem = {
-              price,
-              quantity,
-              cost,
-            };
-            return result;
-          };
-
-          orderBookMsg.asks = rawOrderBookMsg.data
-            .filter((x) => x.side === 'Sell' && idPriceMap.has(x.id))
-            .map((x) => parse(x));
-          orderBookMsg.asks = rawOrderBookMsg.data
-            .filter((x) => x.side === 'Buy' && idPriceMap.has(x.id))
-            .map((x) => parse(x));
-
-          if (rawOrderBookMsg.action === 'delete') {
-            rawOrderBookMsg.data.forEach((x) => {
-              idPriceMap.delete(x.id);
-            });
-          }
-
-          msgCallback(orderBookMsg);
-          break;
-        }
-        case 'trade': {
-          const arr = obj.data as ReadonlyArray<{
-            timestamp: string;
-            symbol: string;
-            side: 'Sell' | 'Buy';
-            size: number;
-            price: number;
-            tickDirection: 'MinusTick' | 'PlusTick' | 'ZeroMinusTick' | 'ZeroPlusTick';
-            trdMatchID: string;
-            grossValue: number;
-            homeNotional: number;
-            foreignNotional: number;
-          }>;
-
-          const tradeMsges: TradeMsg[] = arr.map((rawTradeMsg) => {
-            const market = marketMap.get(arr[0].symbol)!;
-            assert.ok(market);
-
-            const quantity = rawTradeMsg.homeNotional;
-            // rawTradeMsg.foreignNotional === quantity * rawTradeMsg.price
-            assert.ok(
-              Math.abs(1 - rawTradeMsg.foreignNotional / (quantity * rawTradeMsg.price)) < 0.0001,
-            );
-
-            // Check the grossValue field
-            if (market.base === 'BTC' || market.quote === 'BTC') {
-              assert.equal(
-                Math.round(
-                  1e8 *
-                    (market.base === 'BTC'
-                      ? rawTradeMsg.homeNotional
-                      : rawTradeMsg.foreignNotional),
-                ),
-                rawTradeMsg.grossValue,
-              );
             }
 
-            // The field size has different meanings
-            if (market.type === 'Swap') {
-              assert.equal(market.quote, 'USD');
-              if (market.base === 'BTC') {
-                assert.equal(rawTradeMsg.size, rawTradeMsg.foreignNotional);
-              }
-            } else if (market.type === 'Futures') {
-              if (market.base === 'BTC') {
-                assert.equal(rawTradeMsg.size, rawTradeMsg.foreignNotional);
-              } else if (market.id !== 'ETHUSDM20') {
-                assert.equal(rawTradeMsg.homeNotional, rawTradeMsg.size);
-              }
-            }
-
-            const tradeMsg: TradeMsg = {
-              exchange: EXCHANGE_NAME,
-              marketType,
-              pair: market.pair,
-              rawPair: rawTradeMsg.symbol,
-              channel,
-              channelType,
-              timestamp: new Date(rawTradeMsg.timestamp).getTime(),
-              raw: rawTradeMsg,
-              price: rawTradeMsg.price,
-              quantity,
-              side: rawTradeMsg.side === 'Sell',
-              trade_id: rawTradeMsg.trdMatchID,
-            };
-            return tradeMsg;
-          });
-
-          tradeMsges.forEach((x) => msgCallback(x));
-          break;
-        }
-        case 'tradeBin1m':
-        case 'tradeBin5m':
-        case 'tradeBin1h':
-        case 'tradeBin1d': {
-          const rawKlineMsg = obj as {
-            table: string;
-            action: 'partial' | 'insert';
-            data: ReadonlyArray<{
+            msgCallback(orderBookMsg);
+            break;
+          }
+          case 'trade': {
+            const arr = obj.data as ReadonlyArray<{
               timestamp: string;
               symbol: string;
-              open: number;
-              high: number;
-              low: number;
-              close: number;
-              trades: number;
-              volume: number;
-              vwap: number;
-              lastSize: number;
-              turnover: number;
+              side: 'Sell' | 'Buy';
+              size: number;
+              price: number;
+              tickDirection: 'MinusTick' | 'PlusTick' | 'ZeroMinusTick' | 'ZeroPlusTick';
+              trdMatchID: string;
+              grossValue: number;
               homeNotional: number;
               foreignNotional: number;
             }>;
-          };
-          assert.ok(rawKlineMsg.action === 'partial' || rawKlineMsg.action === 'insert');
 
-          const klineMsges = rawKlineMsg.data.map((x) => {
-            const market = marketMap.get(x.symbol)!;
+            const tradeMsges: TradeMsg[] = arr.map((rawTradeMsg) => {
+              const market = marketMap.get(arr[0].symbol)!;
+              assert.ok(market);
 
-            const klineMsg: KlineMsg = {
-              exchange: EXCHANGE_NAME,
-              marketType,
-              pair: market.pair,
-              rawPair: x.symbol,
-              channel,
-              channelType,
-              timestamp: new Date(x.timestamp).getTime(),
-              raw: x,
-              open: x.open,
-              high: x.high,
-              low: x.low,
-              close: x.close,
-              volume: x.homeNotional,
-              quoteVolume: x.foreignNotional,
-              period: PERIOD_NAMES[rawKlineMsg.table.substring('tradeBin'.length)],
+              const quantity = rawTradeMsg.homeNotional;
+              // rawTradeMsg.foreignNotional === quantity * rawTradeMsg.price
+              assert.ok(
+                Math.abs(1 - rawTradeMsg.foreignNotional / (quantity * rawTradeMsg.price)) < 0.0001,
+              );
+
+              // Check the grossValue field
+              if (market.base === 'BTC' || market.quote === 'BTC') {
+                assert.equal(
+                  Math.round(
+                    1e8 *
+                      (market.base === 'BTC'
+                        ? rawTradeMsg.homeNotional
+                        : rawTradeMsg.foreignNotional),
+                  ),
+                  rawTradeMsg.grossValue,
+                );
+              }
+
+              // The field size has different meanings
+              if (market.type === 'Swap') {
+                assert.equal(market.quote, 'USD');
+                if (market.base === 'BTC') {
+                  assert.equal(rawTradeMsg.size, rawTradeMsg.foreignNotional);
+                }
+              } else if (market.type === 'Futures') {
+                if (market.base === 'BTC') {
+                  assert.equal(rawTradeMsg.size, rawTradeMsg.foreignNotional);
+                } else if (market.id !== 'ETHUSDM20') {
+                  assert.equal(rawTradeMsg.homeNotional, rawTradeMsg.size);
+                }
+              }
+
+              const tradeMsg: TradeMsg = {
+                exchange: EXCHANGE_NAME,
+                marketType,
+                pair: market.pair,
+                rawPair: rawTradeMsg.symbol,
+                channel,
+                channelType,
+                timestamp: new Date(rawTradeMsg.timestamp).getTime(),
+                raw: rawTradeMsg,
+                price: rawTradeMsg.price,
+                quantity,
+                side: rawTradeMsg.side === 'Sell',
+                trade_id: rawTradeMsg.trdMatchID,
+              };
+              return tradeMsg;
+            });
+
+            tradeMsges.forEach((x) => msgCallback(x));
+            break;
+          }
+          case 'tradeBin1m':
+          case 'tradeBin5m':
+          case 'tradeBin1h':
+          case 'tradeBin1d': {
+            const rawKlineMsg = obj as {
+              table: string;
+              action: 'partial' | 'insert';
+              data: ReadonlyArray<{
+                timestamp: string;
+                symbol: string;
+                open: number;
+                high: number;
+                low: number;
+                close: number;
+                trades: number;
+                volume: number;
+                vwap: number;
+                lastSize: number;
+                turnover: number;
+                homeNotional: number;
+                foreignNotional: number;
+              }>;
             };
-            return klineMsg;
-          });
+            assert.ok(rawKlineMsg.action === 'partial' || rawKlineMsg.action === 'insert');
 
-          klineMsges.forEach((x) => msgCallback(x));
-          break;
+            const klineMsges = rawKlineMsg.data.map((x) => {
+              const market = marketMap.get(x.symbol)!;
+
+              const klineMsg: KlineMsg = {
+                exchange: EXCHANGE_NAME,
+                marketType,
+                pair: market.pair,
+                rawPair: x.symbol,
+                channel,
+                channelType,
+                timestamp: new Date(x.timestamp).getTime(),
+                raw: x,
+                open: x.open,
+                high: x.high,
+                low: x.low,
+                close: x.close,
+                volume: x.homeNotional,
+                quoteVolume: x.foreignNotional,
+                period: PERIOD_NAMES[rawKlineMsg.table.substring('tradeBin'.length)],
+              };
+              return klineMsg;
+            });
+
+            klineMsges.forEach((x) => msgCallback(x));
+            break;
+          }
+          default:
+            debug(`Unknown channel ${channel}`);
         }
-        default:
-          debug(`Unknown channel ${channel}`);
-      }
+      },
+      [{ op: 'subscribe', args: subscriptions }],
+    );
 
-      // msgCallback(obj);
-    },
-    [{ op: 'subscribe', args: channels }],
-  );
+    // eslint-disable-next-line no-await-in-loop
+    await new Promise((resolve) => setTimeout(resolve, subscriptions.length * 2000)); // send one subscription per 2 seconds
+  }
 }
